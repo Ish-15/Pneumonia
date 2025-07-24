17ff41763e8f2229cf1c7ea786d1fd784a09d9a6
Ish-15 <ishithasandadi@gmail.com>
2025-07-24 15:25:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if uploaded_file is not None:
     # Get the prediction from your model and convert to a diagnosis
     prediction = cnn.predict(processed_image)[0] ### YOUR CODE HERE
     st.write(prediction)
-    pred_diagnosis = "Pneumonia" if prediction == 1 else "Not Pneumonia" ### YOUR CODE HERE
+    pred_diagnosis = "Pneumonia" if prediction >= 0.5 else "Not Pneumonia" ### YOUR CODE HERE
 
     # Display the processed image
     ### YOUR CODE HERE
